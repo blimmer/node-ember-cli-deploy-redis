127d904b416dadf6ef8b2a68ffa1b404ceb37e28
Ben Limmer <ben@benlimmer.com>
2018-11-28 15:11:43 -0700

    Lint

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fetchIndex = require('./fetch');
 
 module.exports = function (keyPrefix, connectionInfo, opts) {
   return function(req, res, next) {
-    return new Bluebird(function (resolve, reject) {
+    return new Bluebird(function (resolve) {
       fetchIndex(req, keyPrefix, connectionInfo, opts).then(function(indexHtml) {
         res.status(200).send(indexHtml);
         resolve();
